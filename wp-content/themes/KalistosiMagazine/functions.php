4fcc4b5674e780c926d7619c19cd9a7007e5b6e0
Łukasz Powązka <l.powazka@livechatinc.com>
2013-08-15 22:54:50 +0200

    updated styling - caption

--- wp-content/themes/KalistosiMagazine/functions.php
+++ wp-content/themes/KalistosiMagazine/functions.php
@@ -7,7 +7,7 @@ define('BLOG_SITE_URL', home_url());
 if(defined('ENVIRONMENT') && ENVIRONMENT=='dev')
 	define('MEDIA_DATE', time());
 else
-	define('MEDIA_DATE', strtotime('2013-08-14 23:20'));
+	define('MEDIA_DATE', strtotime('2013-08-15 22:30'));
 
 
 if(defined('ENVIRONMENT') && ENVIRONMENT=='dev')
