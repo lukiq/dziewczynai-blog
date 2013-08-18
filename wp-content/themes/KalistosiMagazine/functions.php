d818120510bab83162c7fab368f0ca1c924ed4fe
Łukasz Powązka <l.powazka@livechatinc.com>
2013-08-19 01:11:42 +0200

    removed 28px for admin bar; added opacity to it

--- wp-content/themes/KalistosiMagazine/functions.php
+++ wp-content/themes/KalistosiMagazine/functions.php
@@ -214,4 +214,11 @@ function my_custom_page_word()
 //    get_template_part( 'posts', 'standard' );  
 //}  
 
+add_action('get_header', 'my_filter_head');
+
+function my_filter_head() 
+{
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
+
 ?>
