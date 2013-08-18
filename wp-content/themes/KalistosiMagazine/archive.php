ebca3aa549d583570594f32bda14d2e6cdc112f7
Łukasz Powązka <l.powazka@livechatinc.com>
2013-08-18 17:41:01 +0200

    updated archive titles

--- wp-content/themes/KalistosiMagazine/archive.php
+++ wp-content/themes/KalistosiMagazine/archive.php
@@ -7,19 +7,19 @@
 		<?php if(have_posts()): ?>
 		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 		<?php /* If this is a category archive */ if (is_category()) { ?>
-		<h1 class="pagetitle">Posty dla kategorii: '<?php single_cat_title(); ?>'</h1>
+		<h2 class="pagetitle">Posty dla kategorii '<?php single_cat_title(); ?>'</h2>
 		<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-		<h1 class="pagetitle">Posty oznaczone etykietą: '<?php single_tag_title(); ?>'</h1>
+		<h2 class="pagetitle">Posty oznaczone etykietą '<?php single_tag_title(); ?>'</h2>
 		<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-		<h1 class="pagetitle">Posty oznaczone datą: <?php the_time('F jS, Y'); ?></h1>
+		<h2 class="pagetitle">Posty oznaczone datą <?php the_time('F jS, Y'); ?></h2>
 		<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-		<h1 class="pagetitle">Posty z miesiąca <?php the_time('F, Y'); ?></h1>
+		<h2 class="pagetitle">Posty z miesiąca <?php the_time('F, Y'); ?></h2>
 		<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-		<h1 class="pagetitle">Posty z roku <?php the_time('Y'); ?></h1>
+		<h2 class="pagetitle">Posty z roku <?php the_time('Y'); ?></h2>
 		<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-		<h1 class="pagetitle">Posty autora</h1>
+		<h2 class="pagetitle">Posty autora</h2>
 		<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h1 class="pagetitle">Archiwum bloga</h1>
+		<h2 class="pagetitle">Archiwum bloga</h2>
 		<?php } ?>
 <?php 
 
